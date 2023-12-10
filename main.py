c65651cffe0a03e5e3a6aae456c888d68b7dcde3
Shivam Bhawsar <67222848+cho-zen@users.noreply.github.com>
2023-12-11 00:30:01 +0530

    Update main.py -- Upload Bash Reading API

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Body, File, UploadFile
+import base64
 
 app = FastAPI()
 
@@ -21,3 +22,14 @@ async def upload_image(image: UploadFile= File(...)):
     #     f.write(image_file)
 
     return "Done"
+
+
+@app.post("/bash64")
+def upload_bashCode(text:str=Body(...)):
+
+    with open("templates/image.jpg","wb") as file:
+        file.write(base64.b64decode((text)))
+
+
+    return "All Set!"
+
