941578e5532d0d3b0a5f2a6e983cc10cc6937c9e
Shivam Bhawsar <67222848+cho-zen@users.noreply.github.com>
2023-12-11 16:20:58 +0530

    Update main.py -- Upload face_recognition code

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Body, File, UploadFile
 import base64
 import cv2
+import face_recognition
 
 app = FastAPI()
 
@@ -35,3 +36,21 @@ def upload_bashCode(text:str=Body(...)):
     
 
     return {"Shape":image.shape}
+
+
+@app.post("/facebash")
+def upload_facebashcode(text: str=Body(...)):
+
+    with open("templates/image.jpg","wb") as file:
+        file.write(base64.b64decode((text)))
+
+    img = cv2.imread('templates/image.jpg')
+
+    face_loc = face_recognition.face_locations(img)
+    
+    if len(face_loc) == 0:
+        return "0"
+    else:
+        face_enc = face_recognition.face_encodings(img)
+        return str(face_enc[0])
+
