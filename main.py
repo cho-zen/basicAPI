5e02c96a1b5ac054edfd3c0b7cd2435e8cf104c0
Shivam Bhawsar <67222848+cho-zen@users.noreply.github.com>
2023-12-13 10:02:21 +0530

    Update main.py -- Update New Post Method

--- main.py
+++ main.py
@@ -66,8 +66,50 @@ def upload_facebashcode(text: str=Body(...)):
         return str(face_enc[0])
     
 
-@app.post('/uploadcode/{user_number}/{survey}')
-def upload_facecode(user_number,survey,text: str=Body(...)):
+# @app.post('/uploadcode/{user_number}/{survey}')
+# def upload_facecode(user_number,survey,text: str=Body(...)):
+
+#     with open("templates/image.jpg","wb") as file:
+#         file.write(base64.b64decode((text)))
+
+#     img = cv2.imread('templates/image.jpg')
+
+#     face_loc = face_recognition.face_locations(img)
+    
+#     if len(face_loc) == 0:
+#         return "0"
+#     else:
+#         face_enc = face_recognition.face_encodings(img)
+
+#         firebase_config = {
+#         "apiKey": "AIzaSyCUagURc1l3froPMMFLnUHgIs9eOODB9XI",
+#         "authDomain": "survey-application-c9806.firebaseapp.com",
+#         "databaseURL": "https://survey-application-c9806-default-rtdb.firebaseio.com",
+#         "projectId": "survey-application-c9806",
+#         "storageBucket": "survey-application-c9806.appspot.com",
+#         "messagingSenderId": "112280167703",
+#         "appId": "1:112280167703:web:aadeff65cb8141f52d0352"
+#         }
+
+#         firebase = pyrebase.initialize_app(firebase_config)
+
+#             # Get the auth object
+#         auth = firebase.auth()
+
+#         # Get the current user
+#         user = auth.current_user
+
+#         # Get the user's token
+#         token = user
+
+#         # Connect to Database
+#         db = firebase.database()
+#         db.child(f"Data/{user_number}/{survey}/ImageData").push(str(face_enc[0]),token)
+
+#         return "1"
+
+@app.post('/uploadcode/{user_number}/{survey}/{file_name}')
+def upload_facecode(user_number,survey,file_name,text: str=Body(...)):
 
     with open("templates/image.jpg","wb") as file:
         file.write(base64.b64decode((text)))
@@ -77,7 +119,7 @@ def upload_facecode(user_number,survey,text: str=Body(...)):
     face_loc = face_recognition.face_locations(img)
     
     if len(face_loc) == 0:
-        return "0"
+        return 0
     else:
         face_enc = face_recognition.face_encodings(img)
 
@@ -104,6 +146,7 @@ def upload_facecode(user_number,survey,text: str=Body(...)):
 
         # Connect to Database
         db = firebase.database()
-        db.child(f"Data/{user_number}/{survey}/ImageData").push(str(face_enc[0]),token)
+        db.child(f"Data/{user_number}/{survey}/ImageData/{file_name}").set(str(face_enc[0]),token)
 
-        return "1"
+        return str(face_enc[0])
+    
